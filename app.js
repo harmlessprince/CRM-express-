c58bf92326fa6e5791b88ec1b4fc0c81bdbd4bb1
harmlessprince <realolamilekan@gmail>
2021-06-06 17:56:45 +0100

    added cors

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp')
-
-const AppError = require('./utils/AppError');
-const globalErrorHandler = require('./error/GlobalError');
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+const AppError = require("./utils/AppError");
+const globalErrorHandler = require("./error/GlobalError");
 const CompanyRouter = require("./routes/CompanyRoute");
 const UserRouter = require("./routes/UserRoute");
 const AuthRouter = require("./routes/AuthenticationRoute");
-const ProfileRouter = require('./routes/ProfileRoute');
+const ProfileRouter = require("./routes/ProfileRoute");
 const app = express();
+
+//enable cors on all origin
+// app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://127.0.0.1:8080' }));
 //Set security HTTP headers
 app.use(helmet());
 
@@ -26,29 +30,31 @@ if (process.env.NODE_ENV == "development") {
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
-    messages: "Too many requests, please try again in 15 minutes"
+    messages: "Too many requests, please try again in 15 minutes",
 });
 
 //  apply to all requests
-app.use('/api/', limiter);
+app.use("/api/", limiter);
 
 //converts all request to json format
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 /* Data sanitization against xss */
 app.use(xss());
 // Data sanitization against NOSQL query;
 app.use(mongoSanitize());
 //Prevent parameter pollution
-app.use(hpp({
-    whitelist: [
-        'duration',
-        'ratingsQuantity',
-        'ratingsAverage',
-        'maxGroupSize',
-        'difficulty',
-        'price'
-    ]
-}));
+app.use(
+    hpp({
+        whitelist: [
+            "duration",
+            "ratingsQuantity",
+            "ratingsAverage",
+            "maxGroupSize",
+            "difficulty",
+            "price",
+        ],
+    })
+);
 //Serve static files
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
@@ -57,14 +63,13 @@ app.use((req, res, next) => {
 });
 
 //mount auth routes
-app.use('/api/v1', AuthRouter);
+app.use("/api/v1", AuthRouter);
 //mount profile  routes
 app.use("/api/v1/profile", ProfileRouter);
 //mount user routes
 app.use("/api/v1/users", UserRouter);
 //mount company routes
 app.use("/api/v1/companies", CompanyRouter);
-
 
 app.all("*", (req, res, next) => {
     // const err = new Error(`Can't find ${req.originalUrl} on the serve`);
@@ -76,4 +81,5 @@ app.all("*", (req, res, next) => {
 //Global Erro handling middleware
 app.use(globalErrorHandler);
 
+module.exports = app;
 module.exports = app;
